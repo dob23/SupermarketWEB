e19b92817c9091cc12fddfb33b47f8c42016782f
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-30 19:37:46 -0500

    se crea el modelo de paymode

---
+++ SupermarketWEB/Models/PayMode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB.Models
+{
+    public class PayMode
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string? Observation { get; set; } 
+    }
+}
--- SupermarketWEB/obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ SupermarketWEB/obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df68240a2e48f609a7cc2eeaee6c0357b81e8fbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a9cc055ea52fcd87829c1ea224f5f98a0d605f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
